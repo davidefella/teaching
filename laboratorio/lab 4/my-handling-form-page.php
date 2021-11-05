b1a11b680934cc50120db8eb03f4bb7884e61aef
Davide Fella <davide.fella@gmail.com>
2021-11-05 13:17:21 +0100

    New lab

---
+++ laboratorio/lab 4/my-handling-form-page.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>La tua prima form HTML</title>
+		<style>
+			form {
+				margin: 0 auto;
+				width: 400px;
+				padding: 1em;
+				border: 1px solid #ccc;
+				border-radius: 1em;
+			}
+
+			ul {
+				list-style: none;
+				padding: 0;
+				margin: 0;
+			}
+
+			form li + li {
+				margin-top: 1em;
+			}
+
+			label {
+				display: inline-block;
+				width: 90px;
+				text-align: right;
+			}
+
+			input,
+			textarea {
+				font: 1em sans-serif;
+				width: 300px;
+				box-sizing: border-box; 
+				border: 1px solid #999;
+			}
+
+			input:focus,
+			textarea:focus {
+				border-color: #000;
+			}
+
+			textarea {
+				vertical-align: top;
+				height: 5em;
+			}
+
+			.button {
+				padding-left: 90px;
+			}
+
+			button {
+				margin-left: 0.5em;
+			}
+		</style>
+	</head>
+
+	<body>
+		<?php	if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+					
+					/**
+					 *  se validazione campi ok --> Redirect
+					 * 	    header('Location: http://yoursite.com/dashboard');
+    				 *      exit();
+					 *  altrimenti stampa errore messaggio
+					 *   	echo "<br> message error </br>";
+					 */	
+					
+					echo "Sono in Post"; 
+					
+		}
+		?>
+
+		<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+			<ul>	
+				<li>
+					<label for="nome">Nome:</label>
+					<input type="text" id="nome" name="nome_utente" />
+				</li>
+				<li>
+					<label for="cognome">Cognome:</label>
+					<input type="text" id="cognome" name="cognome_utente" />
+				</li>
+				<li>
+					<label for="indirizzo">Indirizzo:</label>
+					<input id="indirizzo" name="indirizzo_utente"></input>
+				</li>
+				<li>
+					<label for="mail">E-mail:</label>
+					<input type="email" id="mail" name="email_utente" />
+				</li>
+				<li class="button">
+					<button type="submit">Invia i tuoi dati</button>
+				</li>
+			</ul>
+		</form>
+	</body>
+</html>
