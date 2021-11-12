c75a7f67e22d290d3a71ab0a909124d70a4cf44b
Davide Fella <davide.fella@gmail.com>
2021-11-12 02:42:16 +0100

    Nuovo materiale

---
+++ IB/Intro_programmazione/php_fondamenti/laboratorio/lab-5/lab-delete-from-array.php
@@ -0,0 +1,12 @@
+<?php
+$delete_item = 'april';
+
+// lista dei mesi in array
+$months = array('jan', 'feb', 'march', 'april', 'may'); // for april, the key is 4
+foreach (array_keys($months, $delete_item) as $key) {
+    unset($months[$key]);
+}
+
+// stampa l'array
+var_dump($months);
+?>
---
+++ IB/Intro_programmazione/php_fondamenti/laboratorio/lab-5/lab-factorial.php
@@ -0,0 +1,24 @@
+<?php
+$num = 8;
+$factorial = 1;
+
+for ($x=$num; $x>=1; $x--)
+{
+    $factorial = $factorial * $x;
+}
+
+echo "The factorial of $num is $factorial";
+
+//Recursive
+function factorial($number) {
+
+    if ($number < 2) {
+        return 1;
+    } else {
+        return ($number * factorial($number-1));
+    }
+}
+
+echo factorial(4);
+
+?>
