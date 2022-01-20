cf6d55b669d1a44bb28251b8567df7585ec9749a
Davide <davidefella@users.noreply.github.com>
2022-01-20 14:14:23 +0100

    Code refactoring

--- IB/Intro_programmazione/python_oop/Items Manager/iterazione-2/soluzione/main.py
+++ IB/Intro_programmazione/python_oop/Items Manager/iterazione-2/soluzione/main.py
@@ -1,17 +1,18 @@
 class Item:
     pay_rate = 0.8 #20% sconto
-    all = []
+    all_items = []
     
     def __init__(self, name: str, price: float, quantity=0):
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
 
+        
+        
         self.name = name
         self.price = price
         self.quantity = quantity
 
-        Item.all.append(self)
-    
+        Item.all_items.append(self)
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
@@ -27,6 +28,7 @@ item3 = Item("Cable", 10, 5)
 item4 = Item("Mouse", 50, 5)
 item5 = Item("Keyboard", 75, 5)
 
-print(Item.all)
-for i in Item.all: 
+print(Item.all_items)
+
+for i in Item.all_items: 
     print(i.name + " - " + str(i.calculate_total_price()))
