631940aacc9305363ff5042ff86602d08376947a
Davide <davidefella@users.noreply.github.com>
2022-01-24 21:48:33 +0100

    Nuovo codice

--- IB/Intro_programmazione/python_oop/teoria/30_class_vs_static_methods/class_static.py
+++ IB/Intro_programmazione/python_oop/teoria/30_class_vs_static_methods/class_static.py
@@ -1,23 +1,30 @@
 '''
 @classmethod è una funzione built-in decorator di Python, è una espressione che viene 
 valutata dopo la definizione della tua funzione di classe. Un metodo di classe riceve
-la classe come primo argomento implicito. 
+la classe come primo argomento implicito ('cls' per convenzione). 
 
 class C: 
     @classmethod
-    def fun(cls, arg1, arg2, arg3...)
-        your code 
+    def myFunction(cls, arg1, arg2, arg3...)
+        my code 
         
-Dove fun è la nostra funzione da convertire in un metodo di classe ritorna un metodo di classe per una funzione 
-Un metodo di classe è un metodo che è legato alla classe e non all'istanza
+Dove myFunction è la mia funzione da convertire in un metodo di classe ritorna un metodo di classe per una funzione 
+Un metodo di classe è un metodo che è legato alla classe (e poi modificarne lo stato) e non all'istanza
 
 
-@staticmethod
+@staticmethod non riceve in maniera implicita nessum argomento 
 
 class C:
     @staticmethod
-    def fun(arg1, arg2, ...):
-
+    def myFunction(arg1, arg2, ...):
+        my code
  
+Un metodo statico non può modoficare lo stato di una classe o accedervi, è presente in una classe
+perché a livello logico può avere senso.
+In generale, possiamo vedere un metodo statico come una utiliy che può servire in più punti del codice
+
+Generalmente utilizziamo un metodo di classe per creare metodi 'factory' mentre utilizziamo metodi 
+statici quando vogliamo creare delle utility che non hanno però bisogno di accedere allo stato 
+di quella particolare classe. 
 
 '''
