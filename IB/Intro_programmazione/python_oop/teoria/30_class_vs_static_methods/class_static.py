6cd1b99d003e71fa1f453d8d88af87512662f585
Davide <davidefella@users.noreply.github.com>
2022-01-24 21:36:19 +0100

    Aggiunte

---
+++ IB/Intro_programmazione/python_oop/teoria/30_class_vs_static_methods/class_static.py
@@ -0,0 +1,23 @@
+'''
+@classmethod è una funzione built-in decorator di Python, è una espressione che viene 
+valutata dopo la definizione della tua funzione di classe. Un metodo di classe riceve
+la classe come primo argomento implicito. 
+
+class C: 
+    @classmethod
+    def fun(cls, arg1, arg2, arg3...)
+        your code 
+        
+Dove fun è la nostra funzione da convertire in un metodo di classe ritorna un metodo di classe per una funzione 
+Un metodo di classe è un metodo che è legato alla classe e non all'istanza
+
+
+@staticmethod
+
+class C:
+    @staticmethod
+    def fun(arg1, arg2, ...):
+
+ 
+
+'''
