95b05e0da43c0b490bce42d603898a9011b3a421
davidefella <davide.fella@gmail.com>
2022-01-26 19:27:11 +0100

    FIx

--- IB/Intro_programmazione/sql_db/iterazione-1/iterazione-1.sql
+++ IB/Intro_programmazione/sql_db/iterazione-1/iterazione-1.sql
@@ -15,6 +15,19 @@ INSERT INTO "ordine" VALUES(7, 'Boscaiola', 'Anna', '17/01/2022 22:10', 8.5);
 INSERT INTO "ordine" VALUES(8, 'Margherita', 'Marco', '17/01/2022 21:00', 5.5);
 INSERT INTO "ordine" VALUES(9, 'Margherita', 'Simona', '17/01/2022 20:30', 5.5);
 
+
+/*
+INSERT INTO "ordine" VALUES(0, 'Margherita', 'Davide', '2022-01-01 21:05', 5.5); 
+INSERT INTO "ordine" VALUES(1, 'Peperoni', 'Davide', '2022-01-01 22:00', 7); 
+INSERT INTO "ordine" VALUES(2, 'Margherita', 'Marco', '2022-01-01 22:05', 5.5); 
+INSERT INTO "ordine" VALUES(3, '4 Formaggi', 'Carla', '2022-01-03 19:15', 8); 
+INSERT INTO "ordine" VALUES(4, 'Diavola', 'Marta', '2022-01-03 19:45', 7.5);
+INSERT INTO "ordine" VALUES(5, 'Diavola', 'Marta', '2022-01-03 20:06', 7.5);
+INSERT INTO "ordine" VALUES(6, 'Margherita', 'Davide', '2022-01-05 20:10', 5.5);
+INSERT INTO "ordine" VALUES(7, 'Boscaiola', 'Anna', '2022-01-05 20:10', 8.5);
+INSERT INTO "ordine" VALUES(8, 'Margherita', 'Marco', '2022-01-07 21:00', 5.5);
+INSERT INTO "ordine" VALUES(9, 'Margherita', 'Simona', '2022-01-07 20:30', 5.5); */
+
 --4 Tutte le colonne dalla tabella "ordine" senza ordinamento o selezioni 
 SELECT * FROM "ordine"
 
