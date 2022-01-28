67c0f7266a9be7db6e6c898042c0658b7ebedf22
davidefella <davide.fella@gmail.com>
2022-01-28 12:13:37 +0100

    Nuovi esercizi

---
+++ IB/Intro_programmazione/sql_db/iterazione-2/it2-to-do.sql
@@ -0,0 +1,41 @@
+/* 1. Rendere l'attributo 'id' della relazione "ordine" una chiave primaria */ 
+
+
+
+/* 2. Rendere il attributo 'id' della relazione "ordine" come autoincrementante */
+
+
+
+/* 3. Crare una relazione "menu" con i seguenti campi: 
+        - id --> numerico 
+        - nome_pizza --> text 
+        - prezzo --> text 
+        - pizza_bianca --> numerico (1 oppure 0)
+        */
+
+
+
+/* 4. Rendere l'attributo id della relazione "menu" una chiave primaria autoincrementante */ 
+
+
+
+/* 5. Elimina tutti i dati dalla relazione "ordine" */
+
+
+
+/* 6. Modificare la relazione "ordine" nel seguente modo: 
+        6.1 Eliminare l'attributo "nome_pizza"
+        6.2 Aggiungere l'attributo "id_pizza" 
+        6.3 Rendere "id_pizza" una chiave esterna verso la chiave primaria della relazione "ordine" */  
+
+
+
+/* 7. Aggiungere i seguenti vincoli sulle relazioni: 
+    7.1 Vincolo 'check' sull'attributo "pizza_bianca" per controllare se il valore inserito è 0 oppure 1 
+    7.2 Vincolo 'check' sull'attributo "prezzo" per controllare se il valore inserito è maggiore di 0
+    7.3 Vincolo 'unique' sull'attributo "nome_pizza" della relazione "menu"
+    7.4 Default su attributo "totale_ordine" pari a 0.00
+
+
+
+/* 8. Inserisci a tuo piacimento 5 record nella relazione "menu" e 10 record nella relazione "ordine" */
---
+++ IB/Intro_programmazione/sql_db/iterazione-3/iterazione-3.sql
@@ -0,0 +1,56 @@
+/* 1. Crea la relazione
+ "cliente" con i seguenti attributi: 
+    - id --> numerico non nullo, chiave primaria
+    - nome --> text 
+    - email --> text 
+    - telefono --> text*/
+
+
+
+/* 2. Crea la relazione
+ "riga_ordine" con i seguenti attributi: 
+    - id --> numerico non nullo, chiave primaria
+    - fk_ordine numero non nullo, chiave esterna verso "ordine" 
+    - fk_riga_menu numerico non nulla, chiave esterna verso "menu"*/
+
+
+
+/* 3. Modificare la relazione
+ "ordine": 
+    - Eliminare la colonna "nome_cliente"
+    - Aggiungere una colonna fk_cliente di tipo id, chiave esterna verso la relazione
+     "cliente" */
+
+
+
+/* 4. Creare una relazione "tessera_punti" con i seguenti attributi: 
+    - id --> numerico chiave primaria 
+    - data_creazione --> timestamp
+    - totale_punti --> numerico non nullo */
+
+
+
+/* 5. Creare una relazione "rider" con i seguenti attributi: 
+    - id --> numerico, chiave primaria 
+    - id_compagnia --> numerico
+*/
+
+
+
+/* 6. Creare una relazione "azienda_consegne": 
+    - id --> mumerico, chiave primaria 
+    - nome_azienda --> varchar
+    - tariffa --> decimal
+*/
+
+
+
+/* 7. Modificare la relazione "ordine" aggiugendo il seguente attributo: 
+    - "id_rider", intero, chiave eserna verso l'id della relazione "azienda_consegne"
+*/
+
+
+
+/* 8. Modificare la relazione "cliente": 
+    - Creare un campo "id_tessera" e renderla chiave primaria verso la relazione
+     "tessera punti" */
