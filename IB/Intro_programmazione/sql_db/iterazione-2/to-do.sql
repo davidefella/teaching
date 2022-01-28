1e05575e514b1514c337f20e02ad5c5d480d330c
davidefella <davide.fella@gmail.com>
2022-01-28 09:45:39 +0100

    Refactoring

---
+++ IB/Intro_programmazione/sql_db/iterazione-2/to-do.sql
@@ -0,0 +1,35 @@
+/* 1. Rendere il campo 'id' della tabella "ordine" una chiave primaria */ 
+
+
+
+/* 2. Rendere il campo 'id' della tabella "ordine" come autoincrementante */
+
+
+
+/* 3. Crare una tabella "menu" con i seguenti campi: 
+        - id --> numerico 
+        - nome_pizza --> text 
+        - prezzo --> text */
+
+
+
+/* 4. Rendere il campo id una chiave primaria autoincrementante */ 
+
+
+
+/* 5. Elimina tutti i dati dalla tabella "ordine" */
+
+
+
+/* 6. Modificare la tabella "ordine" nel seguente modo: 
+        6.1 Eliminare la colonna "nome_pizza"
+        6.2 Aggiungere una colonna "id_pizza" 
+        6.3 Rendere "id_pizza" una chiave esterna verso la chiave primaria della tabella "ordine" */  
+
+
+
+/* 7. Inserisci a tuo piacimento 5 record nella tabella "menu" e 10 record nella tabella "ordine" */
+
+
+
+/* 8.Che criticità vedi? */
