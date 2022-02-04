bd64bc4a59cbd444e81077ca63118a7e018749f4
davidefella <davide.fella@gmail.com>
2022-02-04 12:12:08 +0100

    Esercizi SQL

---
+++ IB/Intro_programmazione/sql_db/esercizi_sql/cinema.sql
@@ -0,0 +1,30 @@
+/*ATTORI (CodAttore, Nome, AnnoNascita, Nazionalità);
+RECITA (CodAttore*, CodFilm*)
+FILM (CodFilm, Titolo, AnnoProduzione, Nazionalità, Regista, Genere) 
+PROIEZIONI (CodProiezione, CodFilm*, CodSala*, Incasso, DataProiezione) 
+SALE (CodSala, Posti, Nome, Città)
+
+
+1- Il nome di tutte le sale di Pescara
+2- Il titolo dei film di Tarantino prodotti dopo il 1998.
+3- Il titolo e la durata dei film di fantascienza statunitensi o francesi prodotti dopo il 1990 
+4- Il titolo dei film di fantascienza giapponesi prodotti dopo il 1990 oppure francesi
+5- I titolo dei film dello stesso regista di “Casablanca” --> NO
+6- Il titolo ed il genere dei film proiettati il giorno di Pescara 2004
+7- Il titolo ed il genere dei film proiettati a Pescara il giorno di Natale 2004
+8- I nomi delle sale di Pescara in cui il giorno di Natale 2004 è stato proiettato un film con
+R.Williams
+9- Il titolo dei film in cui recita Brad Pitt oppure Leonardo DiCaprio
+10- Il titolo dei film in cui recitano Brad Pitt e Leonardo DiCaprio
+11- Per ogni film in cui recita un attore statunitense, il titolo del film e il nome dell’attore
+12- Per ogni film che è stato proiettato a Pescara nel gennaio 2005, il titolo del film e il nome della
+sala.
+13- Il numero di sale di Pescara con più di 60 posti
+14- Il numero totale di posti nelle sale di Pescara
+15- Per ogni città, il numero di sale
+16- Per ogni città, il numero di sale con più di 60 posti
+17- Per ogni regista, il numero di film diretti dopo il 2000
+18- Per ogni regista, l’incasso totale di tutte le proiezioni dei suoi film
+19- Per ogni film di S.Spielberg, il titolo del film, il numero totale di proiezioni a Pescara e l’incasso
+totale
+20- Per ogni regista e per ogni attore, il numero di film del regista con l’attore*/
